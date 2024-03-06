a05f335d3e52069b88405867764afdc78328ecc2
Brucky <bouncebloc95@gmail.com>
2024-03-06 15:52:21 +0100

    [FEATURE] Custom port

--- main.py
+++ main.py
@@ -4,13 +4,17 @@ from payload import Payload
 parser = argparse.ArgumentParser()
 parser.add_argument("--linux",help="Payload targeting Linux",action="store_true")
 parser.add_argument("--windows",help="Payload targeting Windows",action="store_true")
-parser.add_argument("--ip",help="Reverse connection IP Adress")
-parser.add_argument("--port",help="Reverse connection Port")
+parser.add_argument("-p","--port",help="Reverse connection Port",required=True)
 
 args = parser.parse_args()
+port = int(args.port)
+
+if (port < 4097 or port > 65535 ):
+    print("Please specify a port between 4097 and 65535")
+    exit(1)
 
 if args.linux:
-    payload = Payload(platform="Linux")
+    payload = Payload(platform="Linux",port=port)
     payload.permut()
     print(payload)
 
